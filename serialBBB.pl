d73df4d8a0ef6b1e82b97733cc14bec6d7d76760
root <root@ubuntu-armhf.(none)>
2013-11-24 21:59:06 +0000

    Added check for ttyO*, if doesn't exist, create it, if exist, just use it

--- serialBBB.pl
+++ serialBBB.pl
@@ -12,19 +12,50 @@ use Device::SerialPort;
 #################################################################################################################################################
 
 #set UART1 
-`echo ttyO1_armhf.com > /sys/devices/bone_capemgr.9/slots`;
+# check if SLOT exist if no, crate it
+
+my $SLOT1 = checkSlots(1);
+if ($SLOT1 != 7)
+	{
+		print "Slot ttyO1 doesn't exist, will create it !!\n";		
+		`echo ttyO1_armhf.com > /sys/devices/bone_capemgr.9/slots`;
+	}
 print "\nUART1 Tx set done....PIN (9_24)\n";
 print "UART1 Rx set done....PIN (9_26)\n";
 
 
+#set UART2
+my $SLOT2 = checkSlots(2);
+if ($SLOT2 != 7)
+ 	{
+                print "Slot ttyO2 doesn't exist, will create it !!\n";
+		`echo ttyO2_armhf.com > /sys/devices/bone_capemgr.9/slots`;
+	}
+print "\nUART2 Tx set done....PIN (9_21)\n";
+print "UART2 Rx set done....PIN (9_22)\n";
+
+
 #set UART4 
-`echo ttyO4_armhf.com > /sys/devices/bone_capemgr.9/slots`;
+my $SLOT4 = checkSlots(4);
+if ($SLOT4 != 7)
+        {
+		print "Slot ttyO4 doesn't exist, will create it !!\n";
+		`echo ttyO4_armhf.com > /sys/devices/bone_capemgr.9/slots`;
+	}
 print "\nUART4 Tx set done....PIN (9_13)\n";
 print "UART4 Rx set done....PIN (9_11)\n";
 
+
+
+
 ###########################################################################################
 
-# Activate serial connection for UART4, ttyO4:
+
+uart1();
+
+########################################
+sub uart1 {
+# Activate serial connection for UART1, ttyO1:
 my $PORT1 = "/dev/ttyO1";
 my $serialData1;
 
@@ -42,22 +73,52 @@ open( DEV, "<$PORT1" ) || die "Cannot open $PORT1: $_";
 while (1)
         {
                 sleep(1);
-                my $count_out = $ob1->write("Serial port test\n");
+                my $count_out = $ob1->write("Serial port test in UART1\n");
         }
 
 while ( $serialData1 = <DEV> )
         {
-                print"Serial data: $serialData1\n";
+                print"UART1: $serialData1\n";
         }
 
 undef $ob1;
+}
+
+
+sub uart2 {
+# Activate serial connection for UART2, ttyO2:
+my $PORT2 = "/dev/ttyO2";
+my $serialData2;
+
+my $ob2 = Device::SerialPort->new($PORT2) || die "Can't Open $PORT2: $!";
+
+$ob2->baudrate(9600) || die "failed setting baudrate";
+$ob2->parity("none") || die "failed setting parity";
+$ob2->databits(8) || die "failed setting databits";
+$ob2->handshake("none") || die "failed setting handshake";
+$ob2->write_settings || die "no settings";
+$| = 1;
+
+open( DEV, "<$PORT2" ) || die "Cannot open $PORT2: $_";
+
+while (1)
+        {
+                sleep(1);
+                my $count_out = $ob2->write("Serial port test on UART2\n");
+        }
+
+while ( $serialData2 = <DEV> )
+        {
+                print"UART2: $serialData2\n";
+        }
+
+undef $ob2;
+
+}
 
 
 
-
-
-
-
+sub uatr4 {
 # Activate serial connection for UART4, ttyO4:
 my $PORT4 = "/dev/ttyO4"; 
 my $serialData4;
@@ -76,12 +137,40 @@ open( DEV, "<$PORT4" ) || die "Cannot open $PORT4: $_";
 while (1)
 	{
 		sleep(1);
-		my $count_out = $ob4->write("Serial port test\n");
+		my $count_out = $ob4->write("Serial port test on UART4\n");
 	}
 
 while ( $serialData4 = <DEV> ) 
 	{ 
-		print"Serial data: $serialData4\n";								
+		print"UART4: $serialData4\n";								
 	}
 
 undef $ob4;
+
+}
+
+
+################################################## Subroutines #############################
+
+# check if SLOTS exist 
+sub checkSlots 
+        {
+                my $num = shift;                # num of tty we are looking for
+                my $input_file = "/sys/devices/bone_capemgr.9/slots";
+                open SLOTS, "<$input_file"  || die "Can't open $input_file: $!\n";
+
+
+
+
+
+                while (<SLOTS>)
+                        {
+                                if ($_ =~ m/ttyO$num/)
+                                        {
+                                                return 7;
+                                        }
+                        }
+                close (SLOTS);
+        }
+
+
